bb16fcf928c6fd640120e51f3a2c97ac3d2d5bd2
Sahajpal <54036040+Sahajpal@users.noreply.github.com>
2020-06-16 15:59:15 +0530

    merging conflict

--- app/src/main/java/com/example/kapde_wala/ApplicationData.java
+++
@@ -1,9 +0,0 @@
-package com.example.kapde_wala;
-
-final public class ApplicationData {
-    public static String SERVER_IP = "http://124.253.170.144:25565";
-    public static String USER_STATUS = "/user/status";
-    public static String USER_CREATE_ORDER = "/user/createOrder";
-    public static String ENROLMENT = "";
-    public static String USER_CANCEL_ORDER = "/user/cancelOrder";
-}
